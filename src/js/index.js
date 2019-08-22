28f54388dd63aa6c6bf5390c820cd3f1b19c9c58
Lance Smith <lance.matthew.smith@gmail.com>
2019-08-21 20:26:23 -0700

    Dark and light mode switching with local storage support

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,21 @@ import '../css/style';
 window.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
 
-    const toLightMode = () => body.classList.replace('dark-mode', 'light-mode');
-    const toDarkMode = () => body.classList.replace('light-mode', 'dark-mode');
+    if (localStorage.getItem('color-mode') === 'dark') {
+        body.className = 'dark-mode';
+    } else {
+        body.className = 'light-mode';
+    }
 
-    window.darkmode = toDarkMode;
-    window.lightmode = toLightMode;
+    const switchColorMode = () => {
+        if (body.classList.contains('light-mode')) {
+            body.classList.replace('light-mode', 'dark-mode');
+            localStorage.setItem('color-mode', 'dark');
+        } else {
+            body.classList.replace('dark-mode', 'light-mode');
+            localStorage.setItem('color-mode', 'light');
+        }
+    };
+
+    window.changecolor = switchColorMode;
 });
