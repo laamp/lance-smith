63652b0809c73fdeccce38d50f93636c038ce1ad
Lance Smith <lance.matthew.smith@gmail.com>
2019-08-23 16:54:51 -0700

    Section switching functionality

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ import '../css/mobile';
 window.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     const themeSwitcher = document.getElementById('theme-switcher');
+    const sections = document.querySelectorAll('.content-section');
+    const buttons = document.querySelectorAll('nav ul li');
 
     if (localStorage.getItem('color-mode') === 'dark') {
         body.className = 'dark-mode';
@@ -23,5 +25,21 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    const switchButtonSelected = e => {
+        buttons.forEach(btn => btn.className = '');
+        e.target.classList.add('selected-button');
+
+        updateSelectedSection(e.target.innerText);
+    };
+
+    const updateSelectedSection = sectionName => {
+        sections.forEach(section => section.classList.remove('selected'));
+        const selectedSection = document.querySelector(`[data-section="${sectionName}"]`);
+        selectedSection.classList.add('selected');
+    };
+
     themeSwitcher.addEventListener('click', switchColorMode);
+    buttons.forEach(btn => btn.addEventListener('click', switchButtonSelected));
+
+    // debugging below this line
 });
